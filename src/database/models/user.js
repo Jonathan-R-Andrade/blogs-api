86a2e7945f45a0b508eeb13bbe05d3e6c5ea5b09
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-07-13 16:05:24 -0300

    adiciona o modelo "user"

---
+++ src/database/models/user.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      tableName: 'Users',
+      timestamps: false,
+    },
+  );
+
+  return User;
+};
