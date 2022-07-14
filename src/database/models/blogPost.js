71f4121f25e84ff048dfb1429399d45dca8e46e8
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-07-14 18:28:26 -0300

    feat: adiciona o modelo "BlogPost"

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      published: DataTypes.DATE,
+      updated: DataTypes.DATE,
+      userId: { type: DataTypes.INTEGER, foreignKey: true },
+    },
+    {
+      tableName: 'BlogPosts',
+      createdAt: 'published',
+      updatedAt: 'updated',
+    },
+  );
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(
+      models.User, { foreignKey: 'userId', as: 'user' },
+    );
+  };
+
+  return BlogPost;
+};
