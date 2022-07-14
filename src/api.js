77f0524b0bfd188f968a1c6e5200085bf5a36b7b
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-07-14 18:59:26 -0300

    refactor: muda de lugar a validação do token

--- src/api.js
+++ src/api.js
@@ -4,7 +4,6 @@ const errorManager = require('./middlewares/errorManager');
 const loginRouter = require('./routers/loginRouter');
 const userRouter = require('./routers/userRouter');
 const categoriesRouter = require('./routers/categoriesRouter');
-const validateToken = require('./middlewares/validateToken');
 
 // ...
 
@@ -14,9 +13,6 @@ app.use(express.json());
 
 app.use('/login', loginRouter);
 app.use('/user', userRouter);
-
-app.use(validateToken);
-
 app.use('/categories', categoriesRouter);
 
 app.use(errorManager);
--- src/routers/categoriesRouter.js
+++ src/routers/categoriesRouter.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
 const categoryController = require('../controllers/categoryController');
+const validateToken = require('../middlewares/validateToken');
 
 const router = Router();
+
+router.use(validateToken);
 
 router.post('/', categoryController.create);
 router.get('/', categoryController.list);
