df095dbe60e7b206e8f28ab36b503a4deb420863
Jonathan R. Andrade <jonathanreisdeandrade@gmail.com>
2023-01-24 03:06:55 -0300

    docs: adiciona documentação de POST /login e /user

--- src/api.js
+++ src/api.js
@@ -1,16 +1,20 @@
 require('express-async-errors');
 const express = require('express');
+const swaggerUI = require('swagger-ui-express');
 const errorManager = require('./middlewares/errorManager');
 const loginRouter = require('./routers/loginRouter');
 const userRouter = require('./routers/userRouter');
 const categoriesRouter = require('./routers/categoriesRouter');
 const postRouter = require('./routers/postRouter');
+const swaggerPtDoc = require('./swaggerPt.json');
 
 // ...
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/docs/pt', swaggerUI.serve, swaggerUI.setup(swaggerPtDoc));
 
 app.use('/login', loginRouter);
 app.use('/user', userRouter);
