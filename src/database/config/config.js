131b760b0bfe53abaf4abd3b2d5d32fa727c9a57
Jonathan R. Andrade <jonathanreisdeandrade@gmail.com>
2023-02-25 02:19:16 -0300

    refactor: altera configuração do Sequelize

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,32 +1,24 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || 'test';
-
 const suffix = {
-  dev: '-dev',
+  production: '',
   development: '-dev',
   test: '-test',
 };
 
+const DB_SUFFIX = suffix[process.env.NODE_ENV] || suffix.production;
+
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'blogs-api'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '1234',
-  dialect: 'mysql',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || '3306',
+  database: `${process.env.DB_NAME || 'blogs-api'}${DB_SUFFIX}`,
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'P@ssw0rd',
+  dialect: process.env.DB_DIALECT || 'mysql',
   dialectOptions: {
     timezone: 'Z',
   },
   logging: process.env.DEBUG !== 'false',
 };
 
-module.exports = {
-  development: {
-    ...options,
-  },
-  test: {
-    ...options,
-  },
-};
+module.exports = options;
